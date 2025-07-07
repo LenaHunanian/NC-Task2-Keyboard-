c4d0ea7614dc31411a8d7327ef8a78eab20f3bc2
LenaHunanian <lenasss20053112@gmail.com>
2025-07-07 19:42:50 +0400

    Update ViewController.swift added missing method

--- NCTask2/ViewController.swift
+++ NCTask2/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         setupTextField()
+        setupObservers()
         
     }
     
